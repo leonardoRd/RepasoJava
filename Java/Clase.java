934bb0f7270840ab8c1ff12f7aad51c748afd28a
Leonardo Gaitan <gaitanleonardo6@gmail.com>
2023-05-14 16:59:16 -0300

    First commit

---
+++ Java/Clase.java
@@ -0,0 +1,10 @@
+package Java;
+
+public class Clase {
+    
+    public int num;
+
+    public Clase(int valor){
+        this.num = valor;
+    }
+}
---
+++ Java/Main.java
@@ -0,0 +1,10 @@
+package Java;
+
+public class Main{
+
+    public static void main(String[] args) {
+        Clase c = new Clase(10);
+
+        System.out.println("valor: "+ c);
+    }
+}
